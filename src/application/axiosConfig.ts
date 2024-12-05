8ba0a85b6a836c04d3da2aa30b8933a8da39b958
daoqin <daoqin@douyu.tv>
2024-12-05 10:24:47 +0800

    feat: 加密请求

--- src/application/axiosConfig.ts
+++ src/application/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://82.156.64.202:8080', // Replace with your backend IP and port
+  baseURL: 'https://lightline.space:8080', // Replace with your backend IP and port
   withCredentials: false, // Include credentials for cross-origin requests if needed
 });
 
